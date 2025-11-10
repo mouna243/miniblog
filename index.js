20b7c7c1c06c3378e556964ccadf93943062f9a5
mouna243 <mounasaoui2108@gmail.com>
2025-11-10 01:39:58 +0100

    les fonctions utilitaires

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 //  1 - de récupérer tous les Posts, Users et Comments
 
- async function main() {
-    const [PostsRes, userRes, commentres]= await Promise.all([
+async function main() {
+    const [PostsRes, userRes, commentres] = await Promise.all([
         fetch('https://jsonplaceholder.typicode.com/Posts').then(res => res.json()),
         fetch('https://jsonplaceholder.typicode.com/user').then(res => res.json()),
         fetch('https://jsonplaceholder.typicode.com/comments').then(res => res.json())
-        .then (rest => console.log(rest))
+            .then(rest => console.log(rest))
 
-   ] )
-        //    2 - de convertir les trois réponses en tableaux :
-        const posts = PostsRes;
-        const users =  userRes;
-        const comments = commentres;
-        // using map() methode
-        const arrMap = new Map(users.map(user => [id.user ,user]));
+    ])
+    //    2 - de convertir les trois réponses en tableaux :
+    const posts = PostsRes;
+    const users = userRes;
+    const comments = commentres;
+    // using map() methode
+    const arrMap = new Map(users.map(user => [id.user, user]));
 
-        const comment = new Map();
-        comment.forEach(com => {
-            if(!comment.has(com.postId)){
-                // pour cree un nouvelle stock de commentaires de stock
-                comment.set(com.postId || [])
-            }
-            comment.get(comment.postId).push(comment)
+    const comment = new Map();
+    comment.forEach(com => {
+        if (!comment.has(com.postId)) {
+            // pour cree un nouvelle stock de commentaires de stock
+            comment.set(com.postId || [])
         }
+        comment.get(comment.postId).push(comment)
+    }
 
     );
     console.log(comment)
@@ -36,6 +36,23 @@
         published: true, // tous les posts sont publiés dans cet exemple
         comments: commentsByPost.get(post.id) || []
     }));
-        
+    // 4 - Créer les fonctions utilitaires suivantes :
+    // 4-1 sortByUserName() qui permet de trier par utilsateur
 
-}main();
+    function sortByUserName(array) {
+        return [...array].sort((a, b) => a.userName.localeCompare(b.userName));
+    }
+
+    // 4-2 sortByPostTitle() qui permet de trier par le titre du Post
+
+    function sortByPostTitle() {
+        return [...array].sort((a, b) => a.title.localeCompare(b.title));
+
+    }
+
+    // 4-3 getPagePosts(offset, size = 10) qui retourne un nouveau tableau extrait du tableau allPosts à partir de offset et contenant size éléments
+
+    function getPagePosts(array, offset, size = 10) {
+        return array.slice(offset, offset + size);
+    }
+} main();
