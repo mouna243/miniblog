ef35ed96900d5150287cd91fa5f7b10381de5266
mouna243 <mounasaoui2108@gmail.com>
2025-11-10 01:22:37 +0100

    convertir les trois réponses en tableaux

--- index.js
+++ index.js
@@ -8,4 +8,26 @@
         .then (rest => console.log(rest))
 
    ] )
+        //    2 - de convertir les trois réponses en tableaux :
+        const posts = PostsRes;
+        const users =  userRes;
+        const comments = commentres;
+        // using map() methode
+        const arrMap = new Map(users.map(user => [id.user ,user]));
+
+        const comment = new Map();
+        comment.forEach(com => {
+            if(!comment.has(com.postId)){
+                // pour cree un nouvelle stock de commentaires de stock
+                comment.set(com.postId || [])
+            }
+            comment.get(comment.postId).push(comment)
+        }
+
+    );
+    console.log(comment)
+
+    
+        
+
 }main();
